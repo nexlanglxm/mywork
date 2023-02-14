2574d17d11780826cbe932b4dd1e32818c506e68
Neil Anglim <G00425634@atu.ie>
2023-02-14 13:07:46 +0000

    Committing this into github. All the code works.

---
+++ week2/addOne.py
@@ -0,0 +1,7 @@
+# addOne.py
+# Author: Neil Anglim
+# more with numbers
+
+number = int(input("Please enter a number: "))
+newNumber = number + 1
+print (f"{number} plus one is {newNumber}")
---
+++ week2/inputs.py
@@ -0,0 +1,7 @@
+# inputs.py
+# Author: Neil Anglim
+# learning from inputs
+
+name = input("enter your name")
+print("Hello, your name is " + name + "\nNice to meet you")
+
